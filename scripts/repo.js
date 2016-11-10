038168b114d51412cd53733719ea561f060d684c
Colin Lamont <colinlamont@gmail.com>
2016-11-09 16:24:36 -0800

    added deployment to heroku

--- scripts/repo.js
+++ scripts/repo.js
@@ -3,17 +3,14 @@
 
   repos.allRepos = [];
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: 'https://api.github.com/users/chamberi/repos',
-      type: 'GET',
-      headers: {'Authorization': 'token ' + githubToken},
-      success: function (data) {
-        data.forEach(function (obj) {
-          repos.allRepos.push(obj);
-        });
-        callback();
-      }
-    });
+    $.when(
+    $.get('/github/users/chamberi/repos', function(data) {
+      reposObj.allRepos = data;
+    }),
+    $.get('/github/users/chamberi/followers', function(data) {
+      reposObj.followers = data;
+    })
+    ).done(callback);
   };
 
   repos.withTheAttribute = function(myAttr) {
