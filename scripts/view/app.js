8e4ffcbbf35cf139b3d7deb85dfeff5395af5dc6
Colin Lamont <colinlamont@gmail.com>
2016-11-10 11:43:43 -0800

    refactored code to comply with OOP and FP, and MVC structures

---
+++ scripts/view/app.js
@@ -0,0 +1,13 @@
+'use strict';
+(function(module) {
+  var app = {};
+
+  app.renderIndexPage = function() {
+    Article.articles.forEach(function(a){
+      $('#articles').append(a.toHtml('#project-template'));
+    });
+    $('footer').text('The total lines of code is: ' + Article.statsAll());
+  };
+  Article.fetchAll();
+  module.app = app;
+})(window);
